eeaa5417e62dd005bdc77bd7b39ffc3bdeed1110
gonelastvirus <subeshpoudel20@gmail.com>
2022-12-29 23:00:03 +0545

    all change

--- subesh.py
+++ subesh.py
@@ -6,12 +6,12 @@ import time
 ws=websocket.WebSocket()
 while(True):
 	try:
-		ws.connect('ws://127.0.0.1:8000/show/%20subeshpoudel')
+		ws.connect('ws://127.0.0.1:8000/show/subeshpoudel')
 		break
 		
 	except:
 	 print("connecting..........")
 for i in range(1000):
 	time.sleep(2)
-	ws.send(json.dumps({'value':random.randint(100,500),'node':'2', 'token':'xyz123'}))
+	ws.send(json.dumps({'value':random.randint(100,500),'node':random.randint(0,4)}))
 
